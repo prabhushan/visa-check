69d7f3e4f60d645b1ba952fc6bab4bffedf9c058
prabhushan <priyan.prabhu@gmail.com>
2023-03-19 16:45:29 -0400

    Add existing project files to Git

---
+++ cypress/e2e/1-getting-started/visa.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Check Visa Bulletin', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('https://travel.state.gov/content/travel/en/legal/visa-law0/visa-bulletin.html')
+  })
+
+  it('Check Coming soon', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get('.current:nth-child(2) > div > a').should('have.text',"ComingSoon");
+
+   
+  })
+
+})
