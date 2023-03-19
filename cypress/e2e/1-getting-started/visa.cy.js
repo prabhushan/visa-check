ca82d43f2a743630edaf761c65cd797f492f64ab
prabhushan <priyan.prabhu@gmail.com>
2023-03-19 19:54:08 -0400

    Update visa.cy.js

--- cypress/e2e/1-getting-started/visa.cy.js
+++ cypress/e2e/1-getting-started/visa.cy.js
@@ -24,7 +24,7 @@ describe('Check Visa Bulletin', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-    cy.get('.current:nth-child(2) > div > a').should('have.text',"ComingSon");
+    cy.get('.current:nth-child(2) > div > a').should('have.text',"ComingSoon");
 
    
   })
